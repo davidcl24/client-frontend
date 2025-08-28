ca31c136c6ff9c01d251a16f24ccc89165d35352
davidcl24 <davidcl.flor@gmail.com>
2025-08-28 22:53:37 +0200

    added domain images for development

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['es.web.img3.acsta.net'], // agrega aquí tu host externo
+    domains: ['es.web.img3.acsta.net', 'es.web.img2.acsta.net'], // agrega aquí tu host externo
   },
 };
 
